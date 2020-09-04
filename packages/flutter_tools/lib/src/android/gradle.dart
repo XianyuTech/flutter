2b5cfb2188253bda5fd32526b20e7d64e5e44a74
KyleWong <kang.wang1988@gmail.com>
2020-09-04 22:14:03 +0800

    iOS/Android模块构建支持--split-debug-info和--obfuscate。

--- packages/flutter_tools/lib/src/android/gradle.dart
+++ packages/flutter_tools/lib/src/android/gradle.dart
@@ -23,6 +23,7 @@ import '../flutter_manifest.dart';
 import '../globals.dart' as globals;
 import '../project.dart';
 import '../reporting/reporting.dart';
+import '../runner/flutter_command.dart' show FlutterOptions;
 import 'android_sdk.dart';
 import 'gradle_errors.dart';
 import 'gradle_utils.dart';
@@ -315,7 +316,7 @@ Future<void> buildGradleApp({
     command.add('-Pextra-front-end-options=${buildInfo.extraFrontEndOptions.join(',')}');
   }
   if (buildInfo.extraGenSnapshotOptions != null) {
-    command.add('-Pextra-gen-snapshot-options=${buildInfo.extraGenSnapshotOptions}');
+    command.add('-P${FlutterOptions.kExtraGenSnapshotOptions}=${buildInfo.extraGenSnapshotOptions}');
   }
   if (buildInfo.fileSystemRoots != null && buildInfo.fileSystemRoots.isNotEmpty) {
     command.add('-Pfilesystem-roots=${buildInfo.fileSystemRoots.join('|')}');
@@ -343,13 +344,13 @@ Future<void> buildGradleApp({
     command.add('-Pfast-start=true');
   }
   if (androidBuildInfo.buildInfo.splitDebugInfoPath != null) {
-    command.add('-Psplit-debug-info=${androidBuildInfo.buildInfo.splitDebugInfoPath}');
+    command.add('-P${FlutterOptions.kSplitDebugInfoOption}=${androidBuildInfo.buildInfo.splitDebugInfoPath}');
   }
   if (androidBuildInfo.buildInfo.treeShakeIcons) {
     command.add('-Ptree-shake-icons=true');
   }
   if (androidBuildInfo.buildInfo.dartObfuscation) {
-    command.add('-Pdart-obfuscation=true');
+    command.add('-P${FlutterOptions.kDartObfuscationOption}=true');
   }
   command.add(assembleTask);
 
@@ -579,7 +580,14 @@ Future<void> buildGradleAar({
   }
 
   if (androidBuildInfo.buildInfo.extraGenSnapshotOptions?.isNotEmpty ?? false) {
-    command.add("-Pextra-gen-snapshot-options=${androidBuildInfo.buildInfo.extraGenSnapshotOptions.join(',')}");
+    command.add("-P${FlutterOptions.kExtraGenSnapshotOptions}=${androidBuildInfo.buildInfo.extraGenSnapshotOptions.join(',')}");
+  }
+
+  if (androidBuildInfo.buildInfo.splitDebugInfoPath?.isNotEmpty ?? false) {
+    command.add('-P${FlutterOptions.kSplitDebugInfoOption}=${androidBuildInfo.buildInfo.splitDebugInfoPath}');
+  }
+  if (androidBuildInfo.buildInfo.dartObfuscation ?? false) {
+    command.add('-P${FlutterOptions.kDartObfuscationOption}=true');
   }
 
   command.add(aarTask);
